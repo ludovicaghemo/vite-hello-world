4cda3e04713a356475f1ba2883f293f0838cfa5e
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-11-22 13:17:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import Title from './components/Title.vue';
+export default {
+    components:{
+        Title,
+    },
+    data(){
+        return {
+
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<template>
+  <h1>La mia prima app con Vite!</h1>
+  <Title />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <h2>Questa è una prova per inserire un h2</h2>
+</template>
+
+<style>
+</style>
